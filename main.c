661bb674ec5c5f981812ad147de1ca7b4930faec
Mike H <matroxmike@icloud.com>
2019-04-18 19:00:16 +1000

    cosmetic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   GtkWidget *win;
   gtk_init(&argc, &argv);
   win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(win), "Hello there");
+  gtk_window_set_title(GTK_WINDOW(win), "Hello there 1");
   g_signal_connect(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
   gtk_widget_show(win);
   gtk_main();
